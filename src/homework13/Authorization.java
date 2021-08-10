901d3a552f03f6e54f30dc2227697689646d61e6
KGuthrie040 <guthrie7451@gmail.com>
2021-08-10 00:29:57 -0500

    Mis Match Code

--- src/homework13/Authorization.java
+++ src/homework13/Authorization.java
@@ -19,6 +19,10 @@ public class Authorization {
 	 */
 	static void setProperties() {  
 		// TODO
+		System.setProperty("java.security.krb5.realm", "CS.UWM.EDU");
+		System.setProperty("java.security.krb5.kdc", "localhost");
+		
+		System.setProperty("java.security.auth.login.config","src/homework13/jaas.config");
 	}
 	
     public static void main(String[] args) {
@@ -30,12 +34,35 @@ public class Authorization {
         //    entry named "kerberos" in the JAAS login configuration file and 
     	//    to also use the PlaintextCallbackHandler from lecture 12
 	
+    	
+    	LoginContext lc = null;
 	// TODO
+    	try {
+			lc = new LoginContext("kerberos", new PlaintextCallbackHandler());
+		} catch (LoginException | SecurityException e) {
+			System.err.println("Cannot create LoginContext. " + e.getMessage());
+			System.exit(-1);
+		}
 
         // 2. attempt authentication (you must stop the program if an exception is thrown)
 	
 	// TODO
+    	try {
+			lc.login();
+		} catch (LoginException e) {
+			System.err.println("Authentaction Failed. " + e.getMessage());
+			System.exit(-1);
+		}
+		Subject sub = lc.getSubject();
 
+		System.out.println("Principals [" + sub.getPrincipals().iterator().next()+ "] have logged in");
+		
+		try {
+			lc.logout();
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         System.out.println("Authentication succeeded!");
 
         // 3. now try to execute the Action on behalf of the authenticated Subject
